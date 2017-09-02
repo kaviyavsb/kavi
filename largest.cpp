48627696859275c753a021f2063ec69028cb67a0
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2017-09-02 17:43:06 +0530

    Create largest.cpp

---
+++ largest.cpp
@@ -0,0 +1,17 @@
+# include<iostream.h>
+#include<conio.h>
+int a,b,c;
+{
+if((a>b)&&(a>c))
+{
+cout<<"a is greater";
+}
+elseif((b>a)&&(b>c))
+{
+cout<<"b is greater";
+}
+else
+{
+cout<<"cis greater";
+}
+getch();
