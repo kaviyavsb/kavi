6d7e907f1730f22ca7e89e576967561f4ca923bc
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2017-09-02 08:18:32 +0530

    Create pos or neg or zero.cpp

---
+++ pos or neg or zero.cpp
@@ -0,0 +1,28 @@
+# include<iostream.h>
+#include<conio.h>
+int a=5;
+{
+if(a>0)
+cout<<"this is a positive number";
+}
+else
+{
+cout<<"not a positive number";
+}
+if(a<0)
+{
+cout<<"this is a negative number";
+}
+else
+{
+cout<<"not a negative number";
+}
+if(a=0)
+{
+cout<<"this is a zero";
+}
+else
+{
+cout<<"not a zero";
+}
+getch();
