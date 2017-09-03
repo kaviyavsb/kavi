5412545df11a2fed71492a3cd8e2059a7b6acdb7
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2017-09-03 10:43:05 +0530

    Update and rename leapyear. to leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cin>>a;
+if(a%4==0)
+{
+if(a%100==0)
+[
+if(a%400===0)
+cout<<a<<"is a leap year:'
+else
+cout<<a<<"is not a leap year";
+}
+else
+cout<<a<<"is a leap year";
+}
