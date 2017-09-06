db61f3994fbaa7c1585f2c723c00eb1ba77ed38d
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2017-09-06 22:24:36 +0530

    Create 1to15.cpp

---
+++ 1to15.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+int n=15,m=45,i,sum=0,sum1=0
+{
+for(i=1;i<n;i++)
+{
+sum=sum+i;
+}
+cout<<sum;
+for(i=1;i<m;i++)
+if(i%2!=0)
+{
+sum1=sum1+i;
+}
+else
+{
+}
+cout<<sum1;
+}
+return 0;
+}
