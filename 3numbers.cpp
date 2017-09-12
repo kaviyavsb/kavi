3c53f526aa27960522d77ed24f8537ffacaebba0
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2017-09-12 16:22:57 +0530

    Create 3numbers.cpp

---
+++ 3numbers.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+usiong namespace std;
+int main()
+{
+int a=5,b=6,c=5;
+if((a>b)&&(a>c))
+{
+cout<<"a is the greatest";
+}
+elseif((b>a)&&(b>c))
+{
+cout<<"b is the greatest";
+}
+else
+{
+cout<<"c is the greatest";
+}
+return 0;
+}
