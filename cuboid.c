1c1ff6d07f3e346645568183850a99983c811e2f
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2018-01-21 16:21:53 +0530

    Create cuboid.c

---
+++ cuboid.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+	int l,b,h,volume,surfarea;
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	surfarea=2(l*b)+(b*h)+(h*l));
+	printf("%d\n",volume);
+	printf("%d",surfarea);
+	return 0;
+}
