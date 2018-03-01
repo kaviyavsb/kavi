5e509721eb6bcb5f93bb43e70a0b17102e1f7372
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2018-03-01 16:21:17 +0530

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int kabali,opponent,ans;
+    scanf("%d %d",&kabali,&opponent);
+    ans=opponent-kabali;
+    printf("%d is the members diff between two teams",ans);
+    return 0;
+}
