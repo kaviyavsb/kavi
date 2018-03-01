0123c488d46249731bccbfc98423412712f9c576
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2018-03-01 15:43:30 +0530

    Create lcm.c

---
+++ lcm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,n,i,m,ans=1;
+    scanf("%d %d %d",&a,&b,&n);
+    for(i=1;i<=n;i++)
+    {
+        if((a%i==0)&&(b%i==0))
+       m=i;
+    }
+    ans=(a*b)/m;
+    printf("%d",ans);
+    return 0;
+}
